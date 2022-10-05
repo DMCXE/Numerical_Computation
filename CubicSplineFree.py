90e5ec8b306686ee6ceaacf5a7fa25e6069746e0
DMCXE <1533210298@qq.com>
2022-10-05 21:51:38 +0800

    first version of CubicSplineFree using numpy

---
+++ CubicSplineFree.py
@@ -0,0 +1,85 @@
+import numpy as np
+class CubicSplineFree:
+    def __init__(self,arr1):
+        self.arr1 = arr1
+        self.arr1_x = arr1[:,0]
+        self.arr1_y = arr1[:,1]
+        self.lenth = len(arr1)
+
+    def hn(self):
+        hnn = np.array([])
+        for i in range(0,self.lenth-1):
+            hnn =np.append(hnn,self.arr1_x[i+1]-self.arr1_x[i])
+        return hnn
+
+    def mu(self):
+        mu = np.zeros(1)
+        hn = self.hn()
+        for i in range(1,len(hn)):
+            mu = np.append(mu,hn[i-1]/(hn[i-1]+hn[i]))
+        return mu
+
+    def lam(self):
+        lam = np.zeros(1)
+        hn = self.hn()
+        for i in range(1,len(hn)):
+            lam = np.append(lam,hn[i]/(hn[i-1]+hn[i]))
+        return lam
+
+    def fm(self,i):
+        return (self.arr1_y[i]-self.arr1_y[i+1])/(self.arr1_x[i]-self.arr1_x[i+1])\
+               -(self.arr1_y[i]-self.arr1_y[i-1])/(self.arr1_x[i]-self.arr1_x[i-1])
+
+    def dn(self):
+        dn = np.zeros(1)
+        hn = self.hn()
+        for i in range(1,len(hn)-1):
+            dn = np.append(dn,6*self.fm(i)/(hn[i-1]+hn[i]))
+        return dn
+
+    def TDMA(self,a, b, c, d):
+        try:
+            n = len(d)  # order of tridiagonal square matrix
+            # use a,b,c to create matrix A, which is not necessary in the algorithm
+            A = np.array([[0] * n] * n, dtype='float64')
+            for i in range(n):
+                A[i, i] = b[i]
+                if i > 0:
+                    A[i, i - 1] = a[i]
+                if i < n - 1:
+                    A[i, i + 1] = c[i]
+            # new list of modified coefficients
+            c_1 = [0] * n
+            d_1 = [0] * n
+            for i in range(n):
+                if not i:
+                    c_1[i] = c[i] / b[i]
+                    d_1[i] = d[i] / b[i]
+                else:
+                    c_1[i] = c[i] / (b[i] - c_1[i - 1] * a[i])
+                    d_1[i] = (d[i] - d_1[i - 1] * a[i]) / (b[i] - c_1[i - 1] * a[i])
+            # x: solution of Ax=d
+            x = [0] * n
+            for i in range(n - 1, -1, -1):
+                if i == n - 1:
+                    x[i] = d_1[i]
+                else:
+                    x[i] = d_1[i] - c_1[i] * x[i + 1]
+            x = [round(_, 4) for _ in x]
+            return x
+        except Exception as e:
+            return e
+
+    def Mn(self):
+        a = np.append(self.mu(),0)
+        c = np.append(self.lam(),0)
+        b = 2*np.ones(self.lenth)
+        d = np.append(np.append(np.zeros(1),self.dn()),0)
+        Mn = self.TDMA(a,b,c,d)
+        return Mn
+
+arr = np.array([[1960,180671],[1970,205052],[1980,227225],[1990,249623],[2000,282162],[2010,309327],[2020,329484]])
+
+arr1 = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+a= CubicSplineFree(arr)
+print(a.Mn())
