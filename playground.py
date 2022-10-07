cb139cd52a44eb9f1864eb8ea726f46d7a1832af
DMCXE <1533210298@qq.com>
2022-10-07 21:02:24 +0800

    first version of FitSquares_polynomial using numpy

--- playground.py
+++ playground.py
@@ -1,11 +1,7 @@
 import numpy as np
 arr1 = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
-a = [[1,2]]
-a.append([2,5,6])
-print(len(a))
-ab = np.array([])
-b = np.append(ab,1)
-aa = np.random.random(8)
-print(2*np.ones(8))
-bq = np.append(np.append(np.zeros(1),aa),0)
-print(bq)
+g = np.array([])
+g = np.append(g,[1,2])
+b = np.array([3,4])
+print(b*g)
+print(np.sum(g**2))
